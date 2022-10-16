0e3801d7e9b2f8a6b8962745f81bd56a58092c86
AbdRahmen Amri <abdrahmen.3amri@gmail.com>
2022-10-16 23:53:52 +0100

    add nav to contact + change navClick to async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,27 +11,45 @@ export class AppComponent {
   link2: string = 'Contact';
   link3: string = 'About';
   bye(whereYouWas:string){
-    switch (whereYouWas) {
-      case '/':
-        this.host.nativeElement.querySelector('app-index').classList.add('bye');
-        this.host.nativeElement.querySelector('app-download-btn').classList.add('bye');
-        break;
+    return new Promise((resolve,reject)=>{
+      switch (whereYouWas) {
+        case '/':
+          this.host.nativeElement.querySelector('app-index').classList.add('bye');
+          this.host.nativeElement.querySelector('app-download-btn').classList.add('bye');
+          setTimeout(() => {
+            resolve(true)
+          }, 3000);
+          break;
+        case '/'+this.link2.toLocaleLowerCase():
+          this.host.nativeElement.querySelector('app-contact').classList.add('bye');
+          setTimeout(() => {
+            resolve(true)
+          }, 1500);
+          break;
+      
+        default:
+          break;
+      }
+    })
     
-      default:
-        break;
-    }
 
   }
-  navClick(e: any) {
-    this.bye(this.route.url);
-    if (this.link1 === e) {
-      setTimeout(() => {
-        this.route.navigate(['/' + this.link1.toLocaleLowerCase()]);
-      }, 3000);
+  async navClick(e: any) {
+    await this.bye(this.route.url).then(res => {
+      if(res){
+        if (this.link1 === e) {
+          setTimeout(() => {
+            this.route.navigate(['/' + this.link1.toLocaleLowerCase()]);
+          }, 3000);
+        }
+        if (this.link2 === e)
+          this.route.navigate(['/' + this.link2.toLocaleLowerCase()]);
+        if (this.link3 === e) this.route.navigate(['/']);
+      }
     }
-    if (this.link2 === e)
-      this.route.navigate(['/' + this.link2.toLocaleLowerCase()]);
-    if (this.link3 === e) this.route.navigate(['/']);
+
+    );
+
   }
   constructor(private route: Router, private host: ElementRef) {}
   title = 'AbdRahmen Amri';
