ae5994236c888aa0ac2f9647905dad78e0df52e8
AbdRahmen Amri <abdrahmen.3amri@gmail.com>
2022-10-16 20:05:32 +0100

    About Pages

---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,36 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+  @ViewChild('welcome') welcome!: ElementRef;
+  greeting: any = 'Welcome.';
+  gif:string = '<img src="../../../assets/smoke.gif">';
+
+  greetingToSpan(str: String): string {
+    let i: number = 0;
+    let spans: string = '';
+
+    str.split('').forEach((val) => {
+      i++;
+      spans += '<span style="' + '--i:' + i + '">' + val + '</span>';
+    });
+    return spans;
+  }
+  constructor(private sanitizer: DomSanitizer,private host:ElementRef) {}
+
+  ngOnInit(): void {    
+    this.greeting = this.sanitizer.bypassSecurityTrustHtml(this.greetingToSpan(this.greeting)+this.gif);
+  }
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.host.nativeElement.classList.add("show")
+    }, 1000);
+  }
+
+}
