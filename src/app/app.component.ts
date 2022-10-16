938a66a12c53e227ca660c44fbdd60e419e16b92
AbdRahmen Amri <abdrahmen.3amri@gmail.com>
2022-10-16 20:08:08 +0100

    Add About router & portfolio contact structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'client';
+  link1: string = 'Portfolio';
+  link2: string = 'Contact';
+  link3: string = 'About';
+  bye(whereYouWas:string){
+    switch (whereYouWas) {
+      case '/':
+        this.host.nativeElement.querySelector('app-index').classList.add('bye');
+        this.host.nativeElement.querySelector('app-download-btn').classList.add('bye');
+        break;
+    
+      default:
+        break;
+    }
+
+  }
+  navClick(e: any) {
+    this.bye(this.route.url);
+    if (this.link1 === e) {
+      setTimeout(() => {
+        this.route.navigate(['/' + this.link1.toLocaleLowerCase()]);
+      }, 3000);
+    }
+    if (this.link2 === e)
+      this.route.navigate(['/' + this.link2.toLocaleLowerCase()]);
+    if (this.link3 === e) this.route.navigate(['/']);
+  }
+  constructor(private route: Router, private host: ElementRef) {}
+  title = 'AbdRahmen Amri';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { IndexComponent } from './pages/index/index.component';
+import { DownloadBtnComponent } from './component/download-btn/download-btn.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes = [
+  {path: '',component:IndexComponent},
+  {path: 'portfolio',component:PortfolioComponent},
+  {path: 'contact',component:ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    DownloadBtnComponent,
+    PortfolioComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
