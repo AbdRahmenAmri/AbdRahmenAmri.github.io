cfc835a77c1dc2fb9208653c7f7c970ff0042f1f
AbdRahmen Amri <abdrahmen.3amri@gmail.com>
2022-10-16 23:52:11 +0100

    Contact page with on & out effect

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTelegram, faWhatsapp, faGithub } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  icons:Array<any> = [
+    {icon:faEnvelope,text:'Email',link:'mailto:abdrahmen.3amri@gmail.com'},
+    {icon:faFacebook,text:'Facebook',link:'https://www.facebook.com/abd.rahmen.5811877/'},
+    {icon:faTelegram,text:'Telegram',link:'tel:+21651543503'},
+    {icon:faWhatsapp,text:'Whatsapp',link:'tel:+21651543503'},
+    {icon:faGithub,text:'Github',link:'https://github.com/AbdRahmenAmri'},
+  ] 
 
-  constructor() { }
+  constructor(private host:ElementRef) { }
 
   ngOnInit(): void {
+    this.host.nativeElement.classList.add('show');
   }
 
 }
