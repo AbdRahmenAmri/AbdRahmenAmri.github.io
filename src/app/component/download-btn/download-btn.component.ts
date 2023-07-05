f1e65340bb718423304741d3d285ab4b8cea47d3
AbdRahmenAmri <99622864+AbdRahmenAmri@users.noreply.github.com>
2023-07-05 02:43:08 +0100

    Update download-btn.component.ts
    update CV link

--- src/app/component/download-btn/download-btn.component.ts
+++ src/app/component/download-btn/download-btn.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
   styleUrls: ['./download-btn.component.css']
 })
 export class DownloadBtnComponent implements OnInit {
-  cvLink:URL = new URL('https://drive.google.com/file/d/1iIDnD3BlEIh7wpar5UhLITf4prX3zwV2/view?usp=sharing');
+  cvLink:URL = new URL('https://drive.google.com/file/d/1XIUtUm3wXX9_GekcA8tE9Q9iWOyxc2y0/view?usp=drive_link');
 downloadCV() {
   window.open(this.cvLink,'_blank')
 }
